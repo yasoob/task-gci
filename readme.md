91c5291b1902c1f7fd857d33dfda4ebb01dc251a
M.Yasoob Ullah Khalid ☺ <yasoob.khld@gmail.com>
2015-12-24 02:13:38 +0500

    added readme and modified task

---
+++ readme.md
@@ -0,0 +1,1 @@
+This repository is for Google Code-In. The ```task.py``` file simply asks for your age and then prints a response depending on your age. It requires Python3 to run.
--- task.py
+++ task.py
@@ -1,3 +1,5 @@
 age = int(input('What is your age?  '))
 if age > 20:
   print('You are quite older than me. Welcome boss!')
+else:
+  print('Sorry brother but you are not my boss!')
